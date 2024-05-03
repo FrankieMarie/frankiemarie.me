271a882c7fb5da048f1b499db040a6807f49a831
FrankieMarie <frankiemarie83@gmail.com>
2024-05-02 20:02:11 -0500

    fade text in overview

--- src/pages/Landing/Overview.tsx
+++ src/pages/Landing/Overview.tsx
@@ -1,20 +1,21 @@
-import Bridge from "src/assets/bridge.jpg";
 import { ScrollAnchor } from "src/components/ScrollAnchor";
 import { SectionHeading } from "src/components/SectionHeading";
-import { Image } from "src/components/Image";
+import { SectionWrapper } from "src/components/SectionWrapper";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "src/utils/motion";
 
-export const Overview = () => {
+export const Overview = SectionWrapper(() => {
   return (
     <section id="overview" className="mt-8 px-8 sm:mt-12">
       <ScrollAnchor id="overview" />
       <SectionHeading text="OVERVIEW" subText="a bit about me." />
-      <div className="mx-auto max-w-5xl">
-        <p className="mx-auto mt-4">
+      <motion.div className="mx-auto max-w-5xl" variants={textVariant()}>
+        <motion.p variants={fadeIn("up", "", 0.1, 1)} className="mx-auto mt-4">
           I am a web developer from the Upper Peninsula of Michigan. I have over
           10 years experience working with HTML, CSS, and JavaScript, and over 6
           years professionally. I specialize in building front end applications
           with a focus on design, semantics, accessibility, and user experience.
-        </p>
+        </motion.p>
 
         <p className="mx-auto mt-4">
           Outside of tech, I also enjoy the outdoor activities that the UP has
@@ -22,7 +23,7 @@ export const Overview = () => {
           around with Photoshop, drawing, spray paint art, sculpting, and
           working on my house and truck.
         </p>
-      </div>
+      </motion.div>
     </section>
   );
-};
+}, "overview");
