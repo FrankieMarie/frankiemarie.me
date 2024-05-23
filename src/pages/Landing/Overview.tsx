8b412f91207d73c3fbdca46a9d041ec9e1b6c224
FrankieMarie <frankiemarie83@gmail.com>
2024-05-22 20:51:23 -0500

    update overview

--- src/pages/Landing/Overview.tsx
+++ src/pages/Landing/Overview.tsx
@@ -6,23 +6,13 @@ import { fadeIn, textVariant } from "src/utils/motion";
 
 export const Overview = SectionWrapper(() => {
   return (
-    <section id="overview" className="mt-8 px-8 sm:mt-12">
+    <section id="overview" className="px-8">
       <ScrollAnchor id="overview" />
       <SectionHeading text="OVERVIEW" subText="a bit about me." />
       <motion.div className="mx-auto max-w-5xl" variants={textVariant()}>
         <motion.p variants={fadeIn("up", "", 0.1, 1)} className="mx-auto mt-4">
-          I am a web developer from the Upper Peninsula of Michigan. I have over
-          10 years experience working with HTML, CSS, and JavaScript, and over 6
-          years professionally. I specialize in building front end applications
-          with a focus on design, semantics, accessibility, and user experience.
+          I am a web developer from the Upper Peninsula of Michigan. I specialize in building front end web applications with a focus on design, semantics, accessibility, and user experience. I have been working with TypeScript and modern web frameworks professionally for over 6 years. I particularly enjoy writing CSS and building user interfaces that people enjoy interacting with. If I'm not coding, and the weather is nice, I'm usually out on my kayak, fishing or exploring around. 
         </motion.p>
-
-        <p className="mx-auto mt-4">
-          Outside of tech, I also enjoy the outdoor activities that the UP has
-          to offer, such as kayaking, fishing, and riding my ATV. I also mess
-          around with Photoshop, drawing, spray paint art, sculpting, and
-          working on my house and truck.
-        </p>
       </motion.div>
     </section>
   );
